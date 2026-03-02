1fc60861d5d2e456a58bcb80bb1f40ae23f3651d
AlessioGesuelli <alessio.gesuelli@studenti.unicam.it>
2026-03-02 16:28:14 +0100

    feat: configure EF Core with PostgreSQL, add User model and migrations

--- Eppoi.API/Program.cs
+++ Eppoi.API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Eppoi.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Entity Framework Core + PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
